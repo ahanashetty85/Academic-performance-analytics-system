77c6ef75c614d2fc8d9c536378d532809f3d4eb4
Aneesh8777 <pes1ug23am048@pesu.pes.edu>
2025-11-13 09:52:49 +0530

    Delete pages/api directory

--- pages/api/details.ts
+++
@@ -1,8 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-    const { method } = req;
-    if (method === "GET") {
-        res.status(200).json({ message: "Hello World" });
-    }
-}
